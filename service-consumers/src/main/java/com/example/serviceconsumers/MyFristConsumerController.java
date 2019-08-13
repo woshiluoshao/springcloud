52dfa5e34b94e47e7b485387bf8a608de554d4d5
luoshao <2394775223@qq.com>
2019-08-13 10:38:24 +0800

    1.将springcloud加上配置中心调度。 2.service-spring-config作为整个配置文件目录 3.service-config作为配置服务

--- service-consumers/src/main/java/com/example/serviceconsumers/MyFristConsumerController.java
+++ service-consumers/src/main/java/com/example/serviceconsumers/MyFristConsumerController.java
@@ -1,12 +1,17 @@
 package com.example.serviceconsumers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyFristConsumerController {
+
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     private MyFristConsumer myFristConsumer ;
     /**
@@ -16,7 +21,7 @@ public class MyFristConsumerController {
      */
     @RequestMapping(value="/consumerHelloWorld" , method= RequestMethod.GET)
     public String helloWorld(String name) {
-        String result = myFristConsumer.helloWorld(name) ;
+        String result = myFristConsumer.helloWorld(name)  + ";your port is:" + port;
 
         System.out.println("消费者:" + result);
         return  result;
